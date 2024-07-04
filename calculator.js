0ae5e574ba82bee586af599caffd7a01202238d5
junfengl95 <junfeng011995@gmail.com>
2024-07-04 10:36:35 +0800

    Calculator.html page design with working display

---
+++ calculator.js
@@ -0,0 +1,44 @@
+let display = document.getElementById('display')
+
+function appendToDisplay(value) {
+    if (display.value === '0'){
+        display.value = ''; // Clear initial 0
+    }
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = '0';
+}
+
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function substract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(operator, num1, num2) {
+    switch (opetator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
